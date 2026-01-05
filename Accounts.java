baa4f2104cc37782e3d2324908b1c76c4325fd41
Suraj Kumar <199711515+surajkumar071@users.noreply.github.com>
2026-01-05 07:43:14 +0530

    Update Accounts.java

--- Accounts.java
+++ Accounts.java
@@ -4,8 +4,7 @@
 
 
 
-Portronics 65W Halley 20K 20000mAh Fast Charging Powerbank for Android Smartphones, iPhones, ipads, Tablets & Macbooks, Du
-
+Portronics 65W Halley 20K 20000mAh Fast Charging Powerbank for Android Smartphones, iPhones, ipads, Tab
 
 
 
